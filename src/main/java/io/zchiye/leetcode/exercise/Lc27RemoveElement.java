59e59ef068711271181596eb87fb5a8c0c85805c
zchiye <zchi@zju.edu.cn>
2025-06-06 19:21:30 +0800

    27. 移除元素

---
+++ src/main/java/io/zchiye/leetcode/exercise/Lc27RemoveElement.java
@@ -0,0 +1,43 @@
+package io.zchiye.leetcode.exercise;
+
+import java.util.Arrays;
+
+public class Lc27RemoveElement {
+
+    static class Solution {
+        public int removeElement(int[] nums, int val) {
+            int result = 0;
+            int left = 0;
+            int right = nums.length - 1;
+
+            while (right >= left) {
+                if (nums[left] == val) {
+                    while (right > left && nums[right] == val) {
+                        right--;
+                    }
+                    if (right > left) {
+                        nums[left] = nums[right];
+                        right--;
+                        left++;
+                        result++;
+                    } else {
+                        break;
+                    }
+                } else {
+                    left++;
+                    result++;
+                }
+            }
+
+            return result;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = new int[]{3,2,2,3};
+        System.out.println(solution.removeElement(nums, 3));
+        System.out.println(Arrays.toString(nums));
+    }
+
+}
