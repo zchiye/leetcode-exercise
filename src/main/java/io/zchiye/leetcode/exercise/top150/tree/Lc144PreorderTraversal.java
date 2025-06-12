aa1209b3b789adc56dca03099d2bae7b2a2a928d
zchiye <zchi@zju.edu.cn>
2025-06-12 23:26:08 +0800

    TOP150题单 二叉树类 144. 二叉树的前序遍历 递归法

---
+++ src/main/java/io/zchiye/leetcode/exercise/top150/tree/Lc144PreorderTraversal.java
@@ -0,0 +1,38 @@
+package io.zchiye.leetcode.exercise.top150.tree;
+
+import io.zchiye.utils.TestCaseUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lc144PreorderTraversal {
+
+    static class Solution {
+        public List<Integer> preorderTraversal(TreeNode root) {
+            List<Integer> list = new ArrayList<>();
+            traversal(root, list);
+            return list;
+        }
+
+        public void traversal(TreeNode root, List<Integer> list) {
+            if (root == null) {
+                return;
+            }
+            list.add(root.val);
+            traversal(root.left, list);
+            traversal(root.right, list);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        TreeNode t1 = TreeNode.buildFromArray(new Integer[]{1, null, 2, null, null, 3});
+        List<Integer> e1 = new ArrayList<>(List.of(1, 2, 3));
+        TestCaseUtils.testCase(t1, solution::preorderTraversal, e1);
+
+        TreeNode t2 = TreeNode.buildFromArray(new Integer[]{1, 2, 3, 4, 5, null, 8, null, null, 6, 7, null, null, 9});
+        List<Integer> e2 = new ArrayList<>(List.of(1, 2, 4, 5, 6, 7, 3, 8, 9));
+        TestCaseUtils.testCase(t2, solution::preorderTraversal, e2);
+    }
+}
