8ba48f93016de6ef18bf3b674c416c0c6aac905b
zchiye <zchi@zju.edu.cn>
2025-06-12 23:33:44 +0800

    TOP150题单 二叉树类 144. 二叉树的前序遍历 迭代法

--- src/main/java/io/zchiye/leetcode/exercise/top150/tree/Lc144PreorderTraversal.java
+++ src/main/java/io/zchiye/leetcode/exercise/top150/tree/Lc144PreorderTraversal.java
@@ -4,6 +4,7 @@ import io.zchiye.utils.TestCaseUtils;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class Lc144PreorderTraversal {
 
@@ -23,16 +24,44 @@ public class Lc144PreorderTraversal {
             traversal(root.right, list);
         }
 
+        public List<Integer> preorderTraversalIter(TreeNode root) {
+            List<Integer> list = new ArrayList<>();
+
+            Stack<TreeNode> stack = new Stack<>();
+
+            while (root != null) {
+                list.add(root.val);
+
+                if (root.left != null) {
+                    stack.push(root);
+                    root = root.left;
+                } else if (root.right != null) {
+                    root = root.right;
+                } else {
+                    root = null;
+                    while (!stack.empty()) {
+                        TreeNode temp = stack.pop();
+                        if (temp != null && temp.right != null) {
+                            root = temp.right;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            return list;
+        }
+
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
         TreeNode t1 = TreeNode.buildFromArray(new Integer[]{1, null, 2, null, null, 3});
         List<Integer> e1 = new ArrayList<>(List.of(1, 2, 3));
-        TestCaseUtils.testCase(t1, solution::preorderTraversal, e1);
+        TestCaseUtils.testCase(t1, solution::preorderTraversalIter, e1);
 
         TreeNode t2 = TreeNode.buildFromArray(new Integer[]{1, 2, 3, 4, 5, null, 8, null, null, 6, 7, null, null, 9});
         List<Integer> e2 = new ArrayList<>(List.of(1, 2, 4, 5, 6, 7, 3, 8, 9));
-        TestCaseUtils.testCase(t2, solution::preorderTraversal, e2);
+        TestCaseUtils.testCase(t2, solution::preorderTraversalIter, e2);
     }
 }
