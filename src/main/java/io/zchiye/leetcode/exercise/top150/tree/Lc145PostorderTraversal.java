72e00e02a29f132872495628a6415b7d51ac2e04
zchiye <zchi@zju.edu.cn>
2025-06-14 17:40:18 +0800

    TOP150题单 二叉树类 145. 二叉树的后序遍历 迭代法

--- src/main/java/io/zchiye/leetcode/exercise/top150/tree/Lc145PostorderTraversal.java
+++ src/main/java/io/zchiye/leetcode/exercise/top150/tree/Lc145PostorderTraversal.java
@@ -6,10 +6,10 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Stack;
 
-public class Lc94InorderTraversal {
+public class Lc145PostorderTraversal {
 
     static class Solution {
-        public List<Integer> inorderTraversal(TreeNode root) {
+        public List<Integer> postorderTraversal(TreeNode root) {
             List<Integer> list = new ArrayList<>();
             traversal(root, list);
             return list;
@@ -20,14 +20,15 @@ public class Lc94InorderTraversal {
                 return;
             }
             traversal(root.left, list);
-            list.add(root.val);
             traversal(root.right, list);
+            list.add(root.val);
         }
 
-        public List<Integer> inorderTraversalIter(TreeNode root) {
+        public List<Integer> postorderTraversalIter(TreeNode root) {
             List<Integer> list = new ArrayList<>();
-
             Stack<TreeNode> stack = new Stack<>();
+            TreeNode last = null;
+
             while (root != null) {
                 if (root.left != null) {
                     stack.push(root);
@@ -35,36 +36,44 @@ public class Lc94InorderTraversal {
                     continue;
                 }
 
-                list.add(root.val);
-
                 if (root.right != null) {
+                    stack.push(root);
                     root = root.right;
-                } else {
-                    root = null;
-                    while (!stack.empty()) {
-                        TreeNode temp = stack.pop();
-                        list.add(temp.val);
-                        if (temp.right != null) {
-                            root = temp.right;
-                            break;
-                        }
+                    continue;
+                }
+
+                list.add(root.val);
+                last = root;
+
+                root = null;
+                while (!stack.empty()) {
+                    root = stack.pop();
+                    if (last == root.right || root.right == null) {
+                        list.add(root.val);
+                        last = root;
+                    } else {
+                        stack.push(root);
+                        root = root.right;
+                        break;
                     }
+                }
+                if (stack.empty()) {
+                    break;
                 }
             }
 
             return list;
         }
-
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
         TreeNode t1 = TreeNode.buildFromArray(new Integer[]{1, null, 2, null, null, 3});
-        List<Integer> e1 = new ArrayList<>(List.of(1, 3, 2));
-        TestCaseUtils.testCase(t1, solution::inorderTraversalIter, e1);
+        List<Integer> e1 = new ArrayList<>(List.of(3, 2, 1));
+        TestCaseUtils.testCase(t1, solution::postorderTraversal, e1);
 
         TreeNode t2 = TreeNode.buildFromArray(new Integer[]{1, 2, 3, 4, 5, null, 8, null, null, 6, 7, null, null, 9});
-        List<Integer> e2 = new ArrayList<>(List.of(4, 2, 6, 5, 7, 1, 3, 9, 8));
-        TestCaseUtils.testCase(t2, solution::inorderTraversal, e2);
+        List<Integer> e2 = new ArrayList<>(List.of(4, 6, 7, 5, 2, 9, 8, 3, 1));
+        TestCaseUtils.testCase(t2, solution::postorderTraversalIter, e2);
     }
 }
