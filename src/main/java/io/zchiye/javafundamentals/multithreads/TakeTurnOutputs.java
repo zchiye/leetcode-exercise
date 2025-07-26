fa1658d16a8f750b2df38d97e42380cb80637b9f
zchiye <zchi@zju.edu.cn>
2025-07-26 21:38:13 +0800

    java基础 多线程 b站 BV1jG3LzSEsy 多线程轮流打印

--- src/main/java/io/zchiye/javafundamentals/multithreads/TakeTurnOutputs.java
+++ src/main/java/io/zchiye/javafundamentals/multithreads/TakeTurnOutputs.java
@@ -1,18 +1,21 @@
 package io.zchiye.javafundamentals.multithreads;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class TakeTurnOutputs {
 
-    private static void takeTurnOutput(int max, int threadNum) throws InterruptedException {
+    private static void takeTurnOutput(int max, int threadNum, List<Integer> resultList) throws InterruptedException {
         AtomicInteger target = new AtomicInteger(0);
         AtomicInteger token = new AtomicInteger(0);
         for (int i = 0; i < threadNum; i++) {
-            AtomicInteger threadMark = new AtomicInteger(i);
+            int threadMark = i;
             Thread thread = new Thread(() -> {
                 while (target.get() <= max) {
-                    if (token.get() % threadNum == threadMark.get()) {
-                        System.out.println("Thread " + threadMark.get() + " : " + target.getAndIncrement());
+                    if (token.get() % threadNum == threadMark) {
+//                        System.out.println("Thread " + threadMark + " : " + target.getAndIncrement());
+                        resultList.add(target.getAndIncrement());
                         token.incrementAndGet();
                     }
                 }
@@ -22,7 +25,21 @@ public class TakeTurnOutputs {
     }
 
     public static void main(String[] args) throws InterruptedException {
-        takeTurnOutput(300, 5);
+        long startMs = System.currentTimeMillis();
+        List<Integer> resultList = new ArrayList<>();
+        takeTurnOutput(10000000, 5, resultList);
+        long endMs = System.currentTimeMillis();
+        System.out.println("CostMs: " + (endMs - startMs));
+        System.out.println(checkResultList(resultList));
+    }
+
+    private static boolean checkResultList(List<Integer> list) {
+        for (int i = 1; i < list.size(); i++) {
+            if (list.get(i) - list.get(i - 1) != 1) {
+                return false;
+            }
+        }
+        return true;
     }
 
 }
